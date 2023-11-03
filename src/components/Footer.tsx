3057790f816f351a3bb8edc9961e77541c1bd592
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-03 10:09:55 +0300

    Create footer and change styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,9 @@
 import CoctailLogo from "../images/coctail_logo.png";
+import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
+import { BiLogoTelegram } from "react-icons/bi";
+import { MdWifiCalling3 } from "react-icons/md";
 
 const Footer = () => {
-    const titlesItems = [
-        { title: 'Информация' },
-        { title: 'Мой кабинет' },
-        { title: 'Контактная информация' },
-        { title: 'Соц. сети' }
-    ];
-
     const infoItems = [
         { description: 'Главная' },
         { description: 'Акции' },
@@ -26,31 +22,52 @@ const Footer = () => {
         { description: "Моя информация" }
     ];
 
+    const socialMediaIcons = [
+        { icon: <BiLogoTelegram /> },
+        { icon: <AiOutlineInstagram /> },
+        { icon: <AiOutlineMail /> },
+        { icon: <MdWifiCalling3 /> }
+    ];
+
     return (
         <div className="w-[100%] h-[100%] mt-[150px] bg-[#514A7E]">
-            <div className="flex justify-between">
-                {titlesItems.map((item, index) => (
-                    <div key={index}>
-                        <span className="text-[20px] text-[#FFFDF5] font-normal">{item.title}</span>
+            <div className="flex justify-between ml-[135px] mr-[133px]">
+                <div className="flex flex-col mt-[82px]">
+                    <span className="text-[20px] text-[#FFFDF5] font-normal leading-normal mb-[20px]">Информация</span>
+                    {infoItems.map((item, index) => (
+                        <div key={index} className="mb-[10px]">
+                            <span className="text-[12px] text-[#FFFDF5] font-light leading-normal">{item.description}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex flex-col mt-[82px]">
+                    <span className="text-[20px] text-[#FFFDF5] font-normal leading-normal mb-[20px]">Мой кабинет</span>
+                    {cabinetItems.map((item, index) => (
+                        <div key={index} className="mb-[10px]">
+                            <span className="text-[14px] text-[#FFFDF5] font-light leading-normal">{item.description}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex flex-col w-[250px] mt-[82px]">
+                    <span className="text-[20px] text-[#FFFDF5] font-normal leading-normal mb-[20px]">Контактная информация</span>
+                    <p className="text-[14px] text-[#FFFDF5] font-light leading-normal">ИП Вишневский Иван Сергеевич
+                        государственная регистрация №690867884 от 31.07.2020.
+                        Логойским горисполкомом
+                        Защита прав потребителей +375259990755</p>
+                </div>
+                <div className="flex flex-col mt-[82px]">
+                    <span className="text-[20px] text-[#FFFDF5] font-normal leading-normal mb-[20px]">Соц. сети</span>
+                    <div className="flex">
+                        {socialMediaIcons.map((item, index) => (
+                            <span className="mr-[10px] text-[#FFFDF5]" key={index}>{item.icon}</span>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className="mt-[20px]">
-                {infoItems.map((item, index) => (
-                    <div key={index}>
-                        <span className="mt-[10px] text-[12px] text-[#FFFDF5] font-normal">{item.description}</span>
-                    </div>
-                ))}
-            </div>
-            <div>
-                {cabinetItems.map((item, index) => (
-                    <div key={index}>
-                        <span>{item.description}</span>
-                    </div>
-                ))}
-            </div>
-            <div className="flex">
-                <img src={CoctailLogo} alt="" className="w-[84px] h-[84px] flex-shrink-0" />
+                    <span className="text-[14px] text-[#FFFDF5] font-light leading-normal mt-[31px]">+375255990755</span>
+                    <span className="text-[14px] text-[#FFFDF5] font-light leading-normal mt-[32px]">круглосуточно, без выходных</span>
+                </div>
+                <div className="mt-[220px] ml-[97px]">
+                    <img src={CoctailLogo} alt="" className="w-[84px] h-[84px]" />
+                </div>
             </div>
         </div>
     )
