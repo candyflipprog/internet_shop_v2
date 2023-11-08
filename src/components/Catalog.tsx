8695d74308bc6c47159d128d18ea40f649c38ccb
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-08 18:45:40 +0300

    create: catalog component

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const Catalog = () => {
+    return (
+        <div className="flex flex-col">
+            <Header />
+        </div>
+    )
+};
+
+export default Catalog;
