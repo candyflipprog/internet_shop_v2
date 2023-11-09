27ed43867d65674406058e2a64b06eb23a965abb
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-09 23:02:54 +0300

    fix: rendering sub menu items after click menu item

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import NeonLogo from "../images/logo-neon.png";
 
-const LeftSideBar = () => {
+interface MenuItem {
+    id: number;
+    link: string
+};
+
+const LeftSideBar: React.FC = () => {
     const [menuItemId, setMenuItemId] = useState<number | null>(null);
 
-    const menuItems = [
+    const menuItems: MenuItem[] = [
         { id: 1, link: "Женщинам" },
         { id: 2, link: "Мужчинам" },
         { id: 3, link: "Детям" },
@@ -17,33 +22,52 @@ const LeftSideBar = () => {
         { id: 10, link: "Акции" }
     ];
 
-    const subMenuItems = [
-        { itemId: 1},
-        { itemText: "Майки" },
-        { itemText: "Костюмы" },
-        { itemText: "Брюки" },
-        { itemText: "Джинсы" },
-        { itemText: "Юбки" },
-        { itemText: "Шорты" },
-        { itemText: "Свитшот, худи" },
-        { itemText: "Блузки и рубашки" },
-        { itemText: "Пиджаки и джакеты" },
-        { itemText: "Платья и сарафаны" },
-        { itemText: "Верхняя одежда" }
-    ];
+    const subMenuItems: {[key: number]: string[]} = {
+        1: ["Майки",
+            "Костюмы",
+            "Брюки",
+            "Джинсы",
+            "Юбки",
+            "Шорты",
+            "Свитшот, худи",
+            "Блузки и рубашки",
+            "Пиджаки и джакеты",
+            "Платья и сарафаны",
+            "Верхняя одежда"
+        ],
+        2: [
+            "T-shirt",
+            "Штаны"
+        ]
+    };
 
     return (
         <div>
-            <div className={`flex flex-col absolute bg-[#121212] shrink-0 w-72 h-[895px]`}>
+            <div className={`flex flex-col absolute bg-[#121212] shrink-0 w-96 h-[895px]`}>
                 <img src={NeonLogo} alt="neon-logo" className="w-[118px] h-[118px] ml-0" />
                 <ol className="flex flex-col text-[#FFFDF5] ml-[18px]">
-                    {menuItems.map((item, index) => (
-                        <li key={index} className="mt-5 text-[14px] cursor-pointer font-light" onClick={() => setMenuItemId(item.id)}>{item.link}</li>
+                    {menuItems.map((item) => (
+                        <li
+                            key={item.id}
+                            className="mt-5 text-[14px] cursor-pointer font-light"
+                            onClick={() => setMenuItemId(item.id)}
+                        >
+                            {item.link}
+                        </li>
                     ))}
                 </ol>
+                {menuItemId !== null && (
+                    <ol className="fixed py-32 px-44 text-[#FFFDF5] ml-[18px]">
+                        {subMenuItems[menuItemId].map((subMenuItem, index) => (
+                            <li key={index} className="mt-2 text-[14px] cursor-pointer font-light">
+                                {subMenuItem}
+                            </li>
+                        ))}
+                    </ol>
+                )}
             </div>
         </div>
-    )
+    );
 };
 
 export default LeftSideBar;
