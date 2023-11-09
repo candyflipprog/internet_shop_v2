9f981b164dd8b01743995903464c25487e42c0e6
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-09 20:03:27 +0300

    Add NavBar menu for route

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import About from "./About";
+import Cooperation from "./Cooperation";
+import Faq from "./Faq";
+import Footer from "./Footer";
+import Header from "./Header";
+import Invited from "./Invited";
+import Reviews from "./Reviews";
+import Stocks from "./Stocks";
+import WelcomePage from "./WelcomePage";
+
+const NavBar = () => {
+    return (
+        <div>
+            <Header />
+            <WelcomePage />
+            <About />
+            <Stocks />
+            <Reviews />
+            <Faq />
+            <Cooperation />
+            <Invited />
+            <Footer />
+        </div>
+    )
+};
+
+export default NavBar;
