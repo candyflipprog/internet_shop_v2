c8092209b4833298ed6b468fa717f855b16fd1f1
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-09 20:04:28 +0300

    fix: add NavBar, add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./index.css";
-import Header from "./components/Header";
-import WelcomePage from "./components/WelcomePage";
-import About from "./components/About";
-import Stocks from "./components/Stocks";
-import Reviews from "./components/Reviews";
-import Faq from "./components/Faq";
-import Cooperation from "./components/Cooperation";
-import Invited from "./components/Invited";
-import Footer from "./components/Footer";
+import NavBar from './components/NavBar';
 import Catalog from './components/Catalog';
-
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <div className="App mt-12">
-                <Link to="/catalog"/>
-                <Header />
-                <WelcomePage />
-                <About />
-                <Stocks />
-                <Reviews />
-                <Faq />
-                <Cooperation />
-                <Invited />
-                <Footer />
+                <Routes>
+                    <Route path="/" element={<NavBar />}/>
+                    <Route path="/catalog" element={<Catalog />} />
+                </Routes>
             </div>
-        </Router>
+        </BrowserRouter>
     );
 }
 
