e6ba6d91683527781159896497a22af54e31a93c
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-09 13:17:23 +0300

    create: html sctructure

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -2,8 +2,34 @@ import Header from "./Header";
 
 const Catalog = () => {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col font-Roboto">
             <Header />
+            <div>
+                <span className="text-[#121212] text-[40px] font-light">Просмотренные товары</span>
+            </div>
+            <div className="flex flex-col text-[#121212] text-[12px] font-light">
+                <div>
+                    <span>Корзина</span>
+                </div>
+                <div>
+                    <span>Избранное</span>
+                </div>
+                <div>
+                    <span>Просмотренные</span>
+                </div>
+                <div>
+                    <span>Бонусы</span>
+                </div>
+                <div>
+                    <span>Личные данные</span>
+                </div>
+                <div>
+                    <span>История покупок</span>
+                </div>
+                <div>
+                    <span>Выйти</span>
+                </div>
+            </div>
         </div>
     )
 };
