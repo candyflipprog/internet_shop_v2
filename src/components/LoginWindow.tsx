ac05e044bcbc536e0e33a8f30889c7f36cfe0bbb
candyflipprog <dmitriymarlowpierrebourne@gmail.com>
2023-11-09 09:45:36 +0300

    fix: width and height

--- src/components/LoginWindow.tsx
+++ src/components/LoginWindow.tsx
@@ -7,14 +7,14 @@ const LoginWindow = () => {
     const [password, setPassword] = useState<string>('');
 
     return (
-        <div className={`flex flex-col fixed top-64 left-[525px] bg-stone-50 w-[900px] h-[470px] z-50 font-Roboto`}>
+        <div className={`flex flex-col fixed top-64 left-[480px] bg-stone-50 w-[1000px] h-[530px] z-50 font-Roboto}`}>
             <div className={`ml-[113px] mt-[137px] ${showRegistrationWindow ? "hidden" : "flex flex-col"}`}>
                 <div>
                     <span className="text-[#121212] text-[40px] font-light leading-9">Вход в личный кабинет</span>
                 </div>
                 <div className="flex flex-col w-[372px]">
-                    <input type="email" placeholder="Электронная почта" className="border-solid border-b border-[#7D7D7D] outline-none mt-[60px] bg-stone-50" onChange={(event) => setEmail(event.target.value)}/>
-                    <input type="password" placeholder="Пароль" className="border-solid border-b border-[#7D7D7D] outline-none mt-[34px] bg-stone-50" onChange={(event) => setPassword(event.target.value)}/>
+                    <input type="email" placeholder="Электронная почта" className="border-solid border-b border-[#7D7D7D] outline-none mt-[60px] bg-stone-50" onChange={(event) => setEmail(event.target.value)} />
+                    <input type="password" placeholder="Пароль" className="border-solid border-b border-[#7D7D7D] outline-none mt-[34px] bg-stone-50" onChange={(event) => setPassword(event.target.value)} />
                     <label htmlFor="forget_password" className="text-[14px] text-[#514A7E] mt-[23px] cursor-pointer">Не помню пароль</label>
                 </div>
                 <div className="mt-[25px]">
